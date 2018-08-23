ffd900ccb696187692420e779a2e39ca6d799772
ZIDANE <ZIDANE@DESKTOP-ACC2FAE>
2018-08-23 11:02:56 +0700

    Commit Keseluruhan

---
+++ src/Latihan5/MountainTruck.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan5;
+
+/**
+ *
+ * @author ZIDANE
+ */
+public class MountainTruck extends Truck
+{
+    //the Mountaintruck subclass adds one field
+    public int seatHeight;
+    //the Mountaintruck subclass has one constructor
+    public MountainTruck (int startHeight, int startCadence, int startSpeed, int startGear)
+    {
+        super (startCadence, startSpeed, startGear);
+        seatHeight = startHeight;
+    }
+    //the MountainTruck subclass adds one method
+    public void setHeight(int newValue)
+    {
+        seatHeight = newValue;
+    }
+    public static void main(String[]aku)
+    {
+    System.out.println("Mulai Berjalan");
+    MountainTruck MB = new MountainTruck (0,0,0,0);
+    System.out.println("Gear = "+MB.gear);
+    System.out.println("Speed = "+MB.speed);
+    MB.setGear(2);
+    MB.speedUp(2);
+    System.out.println("Gear = "+MB.gear);
+    System.out.println("Speed = "+MB.speed);
+    MB.speedUp(2);
+    System.out.println("Gear = "+MB.gear);
+    System.out.println("Speed = "+MB.speed);
+    }
+}
